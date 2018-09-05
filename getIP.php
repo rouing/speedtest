ee004fd2d049fa5ad40c9143a2cc733e4ae8443c
adolfintel <dosse91@paranoici.org>
2018-09-05 15:46:50 +0200

    Updated getIP.php so it will ignore errors

--- getIP.php
+++ getIP.php
@@ -4,8 +4,11 @@
 	Output from this script is a JSON string composed of 2 objects: a string called processedString which contains the combined IP, ISP, Contry and distance as it can be presented to the user; and an object called rawIspInfo which contains the raw data from ipinfo.io (will be empty if isp detection is disabled).
 	Client side, the output of this script can be treated as JSON or as regular text. If the output is regular text, it will be shown to the user as is.
 */
+error_reporting(0);
 $ip = "";
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
 } elseif (!empty($_SERVER['X-Real-IP'])) {
